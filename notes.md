cd6673b873e09c6af12cd378b506a9e4db608cac
wcolts2000 <wcolts2000@gmail.com>
2019-05-15 12:05:58 -0700

    solution added

---
+++ notes.md
@@ -0,0 +1,18 @@
+# talking to our db with our api
+
+ORM - object relational mapper (knex.js)
+
+- JS object
+  student {
+  name: "",
+  birthdate: "",
+  cohort: []
+  }
+
+DB tables
+student cohorts
+
+- name -cohort_name
+- birthdate
+
+client <> api <> ORM (knex - query builder && sqlite3 - adapter) <> DB
--- roles/roles-router.js
+++ roles/roles-router.js
@@ -1,28 +1,93 @@
-const router = require('express').Router();
+const knex = require("knex");
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
+const knexConfig = require("../knexfile");
+
+const db = knex(knexConfig.development);
+
+router.get("/", (req, res) => {
   // get the roles from the database
-  res.send('Write code to retrieve all roles');
+  db("roles")
+    .then(roles => res.status(200).json(roles))
+    .catch(error => res.status(500).json(error));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // retrieve a role by id
-  res.send('Write code to retrieve a role by id');
+  db("roles")
+    .where({ id: req.params.id })
+    .first() // allows us to return the first object in the set that is returned from the where method
+    .then(role => {
+      if (role) {
+        res.status(200).json(role);
+      } else {
+        res.status(404).json({ message: "Role not found" });
+      }
+    })
+    .catch(error => res.status(500).json(error));
+  // res.send("Write code to retrieve a role by id");
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // add a role to the database
-  res.send('Write code to add a role');
+  const { name } = req.body;
+
+  if (!name) {
+    res.status(400).json({ message: "Must provide a name value" });
+  } else {
+    db("roles")
+      .insert({ name }, "id")
+      .then(ids => {
+        const [id] = ids;
+
+        // NOTE we may just want to return the id only instead of the example below for performance reasons
+        db("roles") // example of nesting our db call to return the entire newly created object instead of just the id
+          .where({ id })
+          .first()
+          .then(role => res.status(201).json(role));
+      })
+      .catch(error => res.status(500).json(error));
+  }
+  // res.send("Write code to add a role");
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update roles
-  res.send('Write code to modify a role');
+  db("roles")
+    .where({ id: req.params.id })
+    .update(req.body, "id")
+    .then(count => {
+      if (count > 0) {
+        db("roles")
+          .where({ id: req.params.id })
+          .first()
+          .then(role => {
+            res.status(200).json(role);
+          });
+      } else {
+        res.status(404).json({ message: "Role not found" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+  // res.send("Write code to modify a role");
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // remove roles (inactivate the role)
-  res.send('Write code to remove a role');
+  db("roles")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: "Role not found" });
+      }
+    })
+    .catch(error => res.status(500).json(error));
+  // res.send("Write code to remove a role");
 });
 
 module.exports = router;
